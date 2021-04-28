7e2117a34c9162a75c6794165d39a3080a3f9dcf
Keno Budde <Clearwood@users.noreply.github.com>
2021-04-29 01:00:05 +0200

    Directly adding liquidity to first pair

--- contracts/flat/SushiSwapMultiSwapperFlat.sol
+++ contracts/flat/SushiSwapMultiSwapperFlat.sol
@@ -235,7 +235,7 @@ contract SushiSwapMultiSwapper {
             path[3] = address(tokenOut);
         }
         path[0] = address(tokenIn);
-        (uint256 amountIn, ) = bentoBox.withdraw(tokenIn, address(this), address(this), 0, shareIn);
+        (uint256 amountIn, ) = bentoBox.withdraw(tokenIn, address(this), UniswapV2Library.pairFor(factory, path[0], path[1], pairCodeHash), 0, shareIn);
         uint256 amount = _swapExactTokensForTokens(amountIn, amountMinOut, path, address(bentoBox));
         (, uint256 share) = bentoBox.deposit(tokenOut, address(bentoBox), to, amount, 0);
         return baseShare.add(share);
@@ -252,7 +252,6 @@ contract SushiSwapMultiSwapper {
         uint256[] memory amounts = UniswapV2Library.getAmountsOut(factory, amountIn, path, pairCodeHash);
         amountOut = amounts[amounts.length - 1];
         require(amountOut >= amountOutMin, "insufficient-amount-out");
-        IERC20(path[0]).safeTransfer(UniswapV2Library.pairFor(factory, path[0], path[1], pairCodeHash), amountIn);
         _swap(amounts, path, to);
     }
 
