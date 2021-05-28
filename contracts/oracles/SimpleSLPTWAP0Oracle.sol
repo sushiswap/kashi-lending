263d1318428158aee4616b4d3f0af302f8f0fc43
Keno <Clearwood@users.noreply.github.com>
2021-05-28 17:21:13 +0200

    Added update option with storage oracle

--- contracts/oracles/SimpleSLPTWAP0Oracle.sol
+++ contracts/oracles/SimpleSLPTWAP0Oracle.sol
@@ -5,6 +5,7 @@ pragma experimental ABIEncoderV2;
 import "../interfaces/IOracle.sol";
 import "@sushiswap/core/contracts/uniswapv2/interfaces/IUniswapV2Factory.sol";
 import '@keydonix/uniswap-oracle-contracts/source/IUniswapV2Pair.sol';
+import { UniswapOracle } from '@keydonix/uniswap-oracle-contracts/source/UniswapOracle.sol';
 import "../libraries/FixedPoint.sol";
 
 // solhint-disable not-rely-on-time
@@ -19,7 +20,7 @@ library BoringMath {
 
 // adapted from https://github.com/Uniswap/uniswap-v2-periphery/blob/master/contracts/examples/ExampleSlidingWindowOracle.sol
 
-contract SimpleSLPTWAP0Oracle is IOracle {
+contract SimpleSLPTWAP0Oracle is IOracle, UniswapOracle {
     using FixedPoint for *;
     using BoringMath for uint256;
     uint256 public constant PERIOD = 5 minutes;
@@ -43,6 +44,36 @@ contract SimpleSLPTWAP0Oracle is IOracle {
         // overflow is desired, casting never truncates
         // cumulative price is in (uq112x112 price * seconds) units so we simply wrap it after division by time elapsed
         return priceCumulative;
+    }
+
+     function updateWithProof(bytes memory data, IUniswapV2Pair uniswapV2Pair, ProofData memory proofData) public returns (bool, uint256) {
+		IUniswapV2Pair pair = abi.decode(data, (IUniswapV2Pair));
+        uint256 historicBlockTimestamp;
+		uint256 historicPriceCumulativeLast;
+		{
+			// Stack-too-deep workaround, manual scope
+			// Side-note: wtf Solidity?
+			uint112 reserve0;
+			uint112 reserve1;
+			uint256 reserveTimestamp;
+			(historicBlockTimestamp, , historicPriceCumulativeLast, reserve0, reserve1, reserveTimestamp) = verifyBlockAndExtractReserveData(uniswapV2Pair, MIN_BLOCKS, MAX_BLOCKS, token1Slot, proofData);
+			uint256 secondsBetweenReserveUpdateAndHistoricBlock = historicBlockTimestamp - reserveTimestamp;
+			// bring old record up-to-date, in case there was no cumulative update in provided historic block itself
+			if (secondsBetweenReserveUpdateAndHistoricBlock > 0) {
+				historicPriceCumulativeLast += secondsBetweenReserveUpdateAndHistoricBlock * uint256(FixedPoint.fraction(reserve1, reserve0)._x);
+			}
+		}
+        uint32 blockTimestamp = uint32(block.timestamp);
+        uint32 timeElapsed = blockTimestamp - uint32(historicBlockTimestamp); // overflow is desired
+        uint256 priceCumulative = _get(pair, blockTimestamp);
+        pairs[pair].priceAverage = FixedPoint
+            .uq112x112(uint224((priceCumulative - historicPriceCumulativeLast) / timeElapsed))
+            .mul(10**18)
+            .decode144();
+        pairs[pair].blockTimestampLast = blockTimestamp;
+        pairs[pair].priceCumulativeLast = priceCumulative;
+
+        return (true, pairs[pair].priceAverage);
     }
 
     function getDataParameter(IUniswapV2Pair pair) public pure returns (bytes memory) {
