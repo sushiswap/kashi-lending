23d734d535c9e6433b6e8d0c73441c1342ef6b29
Keno <Clearwood@users.noreply.github.com>
2021-09-27 18:36:23 -0400

    Package updates

--- settings.js
+++ settings.js
@@ -1,6 +1,20 @@
 module.exports = {
     hardhat: {
         solidity: {
+            compilers: [
+                {
+                  version: "0.7.6",
+                  settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 9000,
+                    },
+                },
+                },
+                {
+                  version: "0.6.12",
+                },
+              ],
             overrides: {
                 "contracts/KashiPair.sol": {
                     version: "0.6.12",
